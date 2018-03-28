4aae1959f47578743a1421f55601d889499dc773
Ben Styles <enquiries@leeds-ebooks.co.uk>
2018-03-28 11:57:51 +0100

    add node() to fluture flowtype libdef

--- flow-typed/set-future-state/fluture.js
+++ flow-typed/set-future-state/fluture.js
@@ -1,12 +1,13 @@
 // @flow
 
 type Cancel = () => void
+type Nodeback<E, V> = (error: ?E, value?: V) => void
 
 declare class Future<E, V> {
   mapRej<M>(f: (error: E) => M): Future<M, V>;
 
   fork(onReject: (error: E) => *, onResolve: (value: V) => void): Cancel;
-  done(callback: (error: ?E, value?: V) => void): Cancel;
+  done(callback: Nodeback<E, V>): Cancel;
 }
 
 declare module 'fluture' {
@@ -15,8 +16,11 @@ declare module 'fluture' {
 
   declare export function tryP<V>(p: () => Promise<V>): Future<*, V>
   declare export function encaseN<A, E, V>(
-    n: (a: A, callback: (error: ?E, value?: V) => void) => void
+    n: (a: A, callback: Nodeback<E, V>) => void
   ): (a: A) => Future<E, V>
+  declare export function node<E, V>(
+    n: (callback: Nodeback<E, V>) => void
+  ): Future<E, V>
 
   declare export function of<V>(value: V): Future<null, V>
   declare export function after<V>(ms: number, value: V): Future<null, V>
